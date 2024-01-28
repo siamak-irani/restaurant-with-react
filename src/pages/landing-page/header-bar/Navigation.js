2261033630879d22b2d9adf7820b35c86152b578
siamak irani <siamakirani1378@gmail.com>
2024-01-28 18:00:58 +0330

    Completing LandingPage

--- src/pages/landing-page/header-bar/Navigation.js
+++ src/pages/landing-page/header-bar/Navigation.js
@@ -2,26 +2,52 @@ import React, { useEffect, useState } from "react";
 
 import classes from "./Navigation.module.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as MenuICon } from "../../../files/icons/menu_FILL0_wght400_GRAD0_opsz48.svg";
+import { ReactComponent as CloseIcon } from "../../../files/icons/close_FILL0_wght400_GRAD0_opsz48.svg";
 
 const Navigation = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setNavIsOpen((prev) => !prev);
+  };
 
   return (
     <nav className={classes["nav"]}>
-      <ul>
+      <div onClick={toggleMenu} className={`${classes["icon"]}`}>
+        {!navIsOpen ? <MenuICon /> : <CloseIcon />}
+      </div>
+      <ul style={{ height: navIsOpen ? "81px" : "0px" }}>
         <li>
-          <Link to="/">خانه</Link>
+          <Link
+            onClick={() => {
+              setNavIsOpen(false);
+            }}
+            to="/"
+          >
+            خانه
+          </Link>
         </li>
         <li>
-          <Link to="/orders">سفارش شما</Link>
+          <Link
+            onClick={() => {
+              setNavIsOpen(false);
+            }}
+            to="/orders"
+          >
+            سفارش شما
+          </Link>
         </li>
+
         <li>
-          <a href="#">خدمات</a>
-        </li>
-        <li>
-          <Link to="categories">منو</Link>
-        </li>
-        <li>
-          <a href="#">درباره ما</a>
+          <Link
+            onClick={() => {
+              setNavIsOpen(false);
+            }}
+            to="categories"
+          >
+            منو
+          </Link>
         </li>
       </ul>
     </nav>
